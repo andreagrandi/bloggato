175b083ff54fbb48305e6500054b611ff638f3b2
Andrea Grandi <a.grandi@gmail.com>
2013-12-03 22:22:01 +0000

    Add BlogPost and Comment models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class BlogPost(models.Model):
+    user = models.ForeignKey(User)
+    title = models.CharField(max_lenght = 120)
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add = True)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User)
+    post = models.ForeignKey(BlogPost)
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add = True)
