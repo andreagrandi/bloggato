64a535ca3c311bdbbf99d87e46dba1969d7d4d7a
Andrea Grandi <a.grandi@gmail.com>
2013-12-03 22:46:47 +0000

    Fix a typo in BlogPost model

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class BlogPost(models.Model):
     user = models.ForeignKey(User)
-    title = models.CharField(max_lenght = 120)
+    title = models.CharField(max_length = 120)
     text = models.TextField()
     date = models.DateTimeField(auto_now_add = True)
 
