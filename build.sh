6ff314a33d9ad535db0228f70f8a7519b72b0aaf
Andrea Grandi <a.grandi@gmail.com>
2013-12-06 21:22:12 +0000

    Make the build script compatible with OSX

--- build.sh
+++ build.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 
-mkdir build
+pip install --no-install -r requirements.txt
 
-pip install --download='./build' --no-install -r requirements.txt
+cp -r src/django-autoload/autoload ./autoload
+cp -r src/django-dbindexer/dbindexer ./dbindexer
+cp -r src/django-nonrel/django ./django
+cp -r src/djangoappengine/djangoappengine ./djangoappengine
+cp -r src/djangotoolbox/djangotoolbox ./djangotoolbox
 
-unzip -q build/django-autoload-*.zip -d build
-unzip -q build/django-dbindexer-*.zip -d build
-unzip -q build/django-nonrel-*.zip -d build
-unzip -q build/djangoappengine-*.zip -d build
-unzip -q build/djangotoolbox-*.zip -d build
-
-cp -r build/django-autoload/autoload ./autoload
-cp -r build/django-dbindexer/dbindexer ./dbindexer
-cp -r build/django-nonrel/django ./django
-cp -r build/djangoappengine/djangoappengine ./djangoappengine
-cp -r build/djangotoolbox/djangotoolbox ./djangotoolbox
-
-#rm -rf build/
+rm -rf src/
