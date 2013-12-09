577922e013083f3a36a115e4e14c228cec7c5a7b
Andrea Grandi <a.grandi@gmail.com>
2013-12-09 13:31:29 +0000

    Add more views to blog

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,20 @@
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return HttpResponse("Hello, world. You're at the Bloggato index.")
+
+@login_required
+def new_post(request):
+    pass
+
+@login_required
+def modify_post(request, id):
+    pass
+
+@login_required
+def delete_post(request, id):
+    pass
+
+def view_post(request, id):
+    pass
