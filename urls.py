8c5f368882e5a1d839ed58bc81b9e498f932590d
Andrea Grandi <a.grandi@gmail.com>
2013-12-09 12:07:56 +0000

    Add initial view to blog

--- urls.py
+++ urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
 from django.views.generic.base import TemplateView
+from blog import views
 
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
-    url(r'^$', TemplateView.as_view(template_name='home.html'))
+    #url(r'^$', TemplateView.as_view(template_name='home.html')),
+    url(r'^$', views.index, name='index')
 )
