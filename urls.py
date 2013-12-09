1101930e717bfbced43c7ee9638b065fb700a881
Andrea Grandi <a.grandi@gmail.com>
2013-12-09 16:44:09 +0000

    Add login/logout urls

--- urls.py
+++ urls.py
@@ -5,4 +5,6 @@ handler500 = 'djangotoolbox.errorviews.server_error'
 urlpatterns = patterns('',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
     url(r'^blog/', include('blog.urls')),
+    ('^login/$', 'django.contrib.auth.views.login'),
+    ('^logout/$', 'django.contrib.auth.views.logout'),
 )
