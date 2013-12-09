f88139c1eb0c89a05090deddf629d7d086337c00
Andrea Grandi <a.grandi@gmail.com>
2013-12-09 13:36:09 +0000

    Add missing urls to blog

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import patterns, url
 from blog import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^new/$', views.new_post, name='post-new'),
+    url(r'^(?P<id>\d+)/$', views.view_post, name='post-detail'),
+    url(r'^(?P<id>\d+)/edit/$', views.modify_post, name='post-modify'),
+    url(r'^(?P<id>\d+)/delete/$', views.delete_post, name='post-delete'),
 )
